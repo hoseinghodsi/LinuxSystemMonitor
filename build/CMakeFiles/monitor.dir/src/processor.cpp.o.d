9dd376b83ada5c28bb1f4797cd2d857edccda57a
Hossein Ghodsi <hoseinghodsi84@gmail.com>
2021-03-14 00:32:53 -0500

    deleting build dir

--- build/CMakeFiles/monitor.dir/src/processor.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/monitor.dir/src/processor.cpp.o: \
- /home/hossein/Udacity/LinuxSystemMonitor/src/processor.cpp \
- /usr/include/stdc-predef.h \
- /home/hossein/Udacity/LinuxSystemMonitor/include/processor.h
