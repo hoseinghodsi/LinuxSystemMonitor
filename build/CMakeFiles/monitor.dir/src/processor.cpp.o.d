56b0315ed70252a5215d056b808cd99563d367b6
Hossein Ghodsi <hoseinghodsi84@gmail.com>
2021-03-14 00:24:03 -0500

    first commit

---
+++ build/CMakeFiles/monitor.dir/src/processor.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/monitor.dir/src/processor.cpp.o: \
+ /home/hossein/Udacity/LinuxSystemMonitor/src/processor.cpp \
+ /usr/include/stdc-predef.h \
+ /home/hossein/Udacity/LinuxSystemMonitor/include/processor.h
